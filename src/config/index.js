1be43675183358878b90d20f4c66a94f1d567c1b
Jhennifer Matias <jhennifercristinematias@gmail.com>
2020-08-03 23:32:50 -0300

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const URL_BACKEND_TOP = window.location.hostname.includes('localhost')
   ? 'http://localhost:8080'
-  : 'https://testeflixjhennifer.herokuapp.com/';
+  : 'https://testeflixjhennifer.herokuapp.com';
 
 export default {
   URL_BACKEND_TOP,
